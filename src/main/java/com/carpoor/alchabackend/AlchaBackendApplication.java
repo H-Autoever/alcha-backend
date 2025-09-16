af22178eb950258b22407e495b3d0325ae4bdf7d
BBSSJJ <bdhbsj@naver.com>
2025-09-16 18:24:15 +0900

    init

---
+++ src/main/java/com/carpoor/alchabackend/AlchaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carpoor.alchabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlchaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlchaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carpoor/alchabackend/AlchaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpoor.alchabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlchaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
