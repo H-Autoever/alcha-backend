52ddc93baa524f1dc41b1ab6b4dd833b9faaa16e
BBSSJJ <bdhbsj@naver.com>
2025-09-23 11:13:59 +0900

    refactor: seperate service logic from controller

--- src/main/java/com/carpoor/alchabackend/sse/SseController.java
+++ src/main/java/com/carpoor/alchabackend/sse/SseController.java
@@ -22,16 +22,6 @@ public class SseController {
     // front와 연결할 엔드 포인트
     @GetMapping(value = "/api/sse/{vehicleId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter connectSse(@PathVariable String vehicleId) {
-
-//        // 연결 즉시 테스트 데이터 전송
-//        try {
-//            emitter.send(SseEmitter.event()
-//                // name 없는 기본 이벤트로 설정하려고 이벤트 타입 제거
-//                .data("SSE 연결 성공! 차량 ID: " + vehicleId));
-//        } catch (Exception e) {
-//            System.out.println("테스트 데이터 전송 실패: " + e.getMessage());
-//        }
-
         return sseService.createEmitter(vehicleId);
     }
 }
