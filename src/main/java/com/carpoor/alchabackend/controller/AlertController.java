959ba297538d26ac675cf75f89ca075d53185afa
BBSSJJ <bdhbsj@naver.com>
2025-10-20 17:15:03 +0900

    fix: fix CORS error

--- src/main/java/com/carpoor/alchabackend/controller/AlertController.java
+++ src/main/java/com/carpoor/alchabackend/controller/AlertController.java
@@ -5,15 +5,13 @@ import com.carpoor.alchabackend.service.AlertCacheService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AlertController {
 
